4a12d8a713050e3ae937f7d2d60f6e588bec5f9c
CChreem <ana.chreem@stone.com.br>
2023-07-22 10:29:32 -0300

    remove unnecessary ussages

--- Calculator/Calculator/Operations/Interfaces/IDivision.cs
+++ Calculator/Calculator/Operations/Interfaces/IDivision.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator.Operations.Interfaces
+﻿namespace Calculator.Operations.Interfaces
 {
     public interface IDivision
     {
